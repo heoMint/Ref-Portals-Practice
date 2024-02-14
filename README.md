e562e5c2807fd3da270964eec11a1d697dae13b5
hdwmumu <wil0110@naver.com>
2024-02-14 15:50:25 +0900

    player name 작성 후 submit 등록 적용

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Ref-Portals-Practice
-`
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [playerName, setPlayerName] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleChange(e) {
+        let player = e.target.value;
+        setPlayerName(player);
+    }
+    function handleClick() {
+      setSubmitted(true);
+    }
+    return (
+        <section id="player">
+            <h2>Welcome {submitted ? playerName: 'Unknown Entity Player'}</h2>
+            <p>
+                <input type="text" onChange={handleChange} value={playerName} />
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
