57267c586eb863966b89f58d4946ebe1af53a33a
hdwmumu <wil0110@naver.com>
2024-02-16 14:15:01 +0900

    ResultModal score 점수 나오도록 수정

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,27 +1,31 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef();
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2); // 소수점 두자리 수 까지만 표시
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
     useImperativeHandle(ref, () => {
         // 객체를 반환해야함
         return {
             open() {
-                dialog.current.showModal()
+                dialog.current.showModal();
             },
         };
     });
 
     return (
-        <dialog ref={dialog} className="result-modal">
-            <h2>You, {result}</h2>
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score : {score}</h2>}
             <p>
                 목표시간은 <strong>{targetTime}초</strong>
             </p>
             <p>
-                <strong>X초</strong> 남았는데 타이머를 멈췄음
+                <strong>{formattedRemainingTime}초</strong> 남았는데 타이머를 멈췄음
             </p>
 
-            <form method="dialog">
+            <form method="dialog" onSubmit={onReset}>
                 <button>닫기</button>
             </form>
         </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,39 +1,45 @@
 import { useState, useRef } from 'react';
 import ResultModal from './ResultModal';
 
-export default function TimerChallenge({ ref, title, targetTime }) {
-    let [timerStarted, setTimerStarted] = useState(false);
-    let [timerExpired, setTimerExpired] = useState(false);
+export default function TimerChallenge({ title, targetTime, remainingTime, onReset }) {
     let timer = useRef();
     let dialog = useRef();
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
+    if (timeRemaining <= 0) {
+        clearInterval(timer.current);
+
+        dialog.current.open();
+    }
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000)
+    }
     function handelStart() {
         timer.current = setInterval(() => {
-            setTimerExpired(true);
-            dialog.current.open();
-        }, 10); // 10 => 10ms
-
-        setTimerStarted(true);
+            setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10); // 업데이트 된 timeRemaining으로 10 밀리초마다 업데이트
+        }, 10); // 0ms
     }
 
     function handelStop() {
-        clearTimeout(timer.current);
+        dialog.current.open();
+        clearInterval(timer.current);
     }
     return (
         <>
-            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} />
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
                     {targetTime} second{targetTime > 1 ? 's' : ''}
                 </p>
                 <p>
-                    <button onClick={timerStarted ? handelStop : handelStart}>
-                        {timerStarted ? 'Stop' : 'Start'} Challenge
+                    <button onClick={timerIsActive ? handelStop : handelStart}>
+                        {timerIsActive ? 'Stop' : 'Start'} Challenge
                     </button>
                 </p>
-                <p className={timerStarted ? ' active' : undefined}></p>
-                {timerStarted ? 'Time is running...' : 'Timer in active'}
+                <p className={timerIsActive ? ' active' : undefined}></p>
+                {timerIsActive ? 'Time is running...' : 'Timer in active'}
             </section>
         </>
     );
